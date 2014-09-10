60c93cd821b574ba9f1f95f1c75c230e797a28b7
ashafir <aviv@optimalfinance.co.il>
2014-09-10 23:59:30 +0300

    added respository

---
+++ BookStoreWebAPI/Models/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace BookStoreWebAPI.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Order> GetAllOrders();
+        IQueryable<Order> GetAllOrdersWithDetails();
+        Order GetOrder(int id);
+    }
+}
---
+++ BookStoreWebAPI/Models/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreWebAPI.Models
+{
+    public class Repository : IRepository
+    {
+        private BookStoreWebAPIContext db;
+
+        public Repository(BookStoreWebAPIContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Order> GetAllOrders()
+        {
+            return db.Orders;
+        }
+
+        public IQueryable<Order> GetAllOrdersWithDetails()
+        {
+            return db.Orders.Include("OrderDetails");
+        }
+
+        public Order GetOrder(int id)
+        {
+            return db.Orders.Include("OrderDetails.Book")
+                .FirstOrDefault(order => order.Id == id);
+        } 
+    }
+}
